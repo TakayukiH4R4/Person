dbac9d518775383913e6ee3c14a85a16e6cd9788
TakayukiH4R4 <sthaikgaoytuok4i@gmail.com>
2024-11-25 21:58:50 +0900

    演習７終了

---
+++ src/jp/co/Person/action/Robot.java
@@ -0,0 +1,19 @@
+package jp.co.Person.action;
+
+public class Robot {
+
+	public String name = null;
+
+	public void talk() {
+		System.out.println(this.name + "が話す");
+	}
+	public void walk() {
+		System.out.println(this.name + "が歩く");
+	}
+	public void run() {
+		System.out.println(this.game + "が走る");
+	}
+
+
+
+}
