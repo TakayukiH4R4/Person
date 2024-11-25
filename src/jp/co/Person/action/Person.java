2bbfc64b44042e34c3aff3db7c7c52553ad5d3a8
TakayukiH4R4 <sthaikgaoytuok4i@gmail.com>
2024-11-25 21:39:04 +0900

    演習２終了

---
+++ src/jp/co/Person/action/Person.java
@@ -0,0 +1,10 @@
+package jp.co.Person.action;
+
+public class Person {
+
+	public String name = null;
+	public int age = 0;
+
+}
+
+
--- src/jp/co/Person/action/Test.java
+++ src/jp/co/Person/action/Test.java
@@ -2,4 +2,37 @@ package jp.co.Person.action;
 
 public class Test {
 
+	public static void main(String[] args) {
+
+		Person taro = new Person();
+		taro.name = "山田太郎";
+		taro.age = 20;
+
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+
+
+		Person kimura = new Person();
+		kimura.name = "木村次郎";
+		kimura.age = 18;
+
+		System.out.println(kimura.name);
+		System.out.println(kimura.age);
+
+		Person suzuki = new Person();
+		suzuki.name = "鈴木花子";
+		suzuki.age = 16;
+
+		System.out.println(suzuki.name);
+		System.out.println(suzuki.age);
+
+		Person hara = new Person();
+		hara.name = "原崇之";
+		hara.age = 28;
+
+		System.out.println(hara.name);
+		System.out.println(hara.age);
+
+
+	}
 }
